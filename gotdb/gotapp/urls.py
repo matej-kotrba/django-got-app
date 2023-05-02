fb7e0c8ba1b340b9fdbc0e98abe3843ce1c14674
Matěj Kotrba <77980040+matej-kotrba@users.noreply.github.com>
2023-05-02 13:19:26 +0200

    Initial commit

---
+++ gotdb/gotapp/urls.py
@@ -0,0 +1,9 @@
+# Z vestavěného modulu django.urls importuje funkce path
+from django.urls import path
+#Ze stejné složky importuje modul views
+from . import views
+
+# Seznam (list) namapovaných cest -URL adres
+urlpatterns = [
+
+]
